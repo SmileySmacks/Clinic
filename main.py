954d8a55446d270a5b44446ec1e144fe45a4a36f
Your Name <you@example.com>
2022-09-28 09:30:53 -0500

    Created tables within MySQL DB using PyCharm.

--- main.py
+++ main.py
@@ -27,6 +27,55 @@ def create_database(connection, query):
         print(f"Error: '{err}'")
 
 
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+
+create_table1 = """
+create table Admissions(
+Patient_ID int primary key,
+Patient_Name varchar(30) not null,
+Patient_DOB varchar(10) not null,
+Patient_Insurance varchar(20) not null,
+Patient_Phone varchar(25) not null,
+Patient_Email varchar(30) not null,
+Patient_Reason varchar(40) not null);"""
+
+create_table2 = """
+create table Anesthetics_Staff(
+Employee_ID int primary key,
+Employee_Name varchar(30) not null,
+Employee_Education varchar(20) not null,
+Employee_Hiring_Date varchar(10) not null,
+Employee_Salary varchar(15) not null,
+Employee_Schedule varchar(10) not null,
+Employee_eBenefits varchar(3) not null);"""
+
+create_table3 = """
+create table Cardiology_Staff(
+Employee_ID int primary key,
+Employee_Name varchar(30) not null,
+Employee_Education varchar(20) not null,
+Employee_Hiring_Date varchar(10) not null,
+Employee_Salary varchar(15) not null,
+Employee_Schedule varchar(10) not null,
+Employee_eBenefits varchar(3) not null);"""
+
+create_table4 = """
+create table September_Cardiology_Appointments(
+Appointment_ID int primary key,
+Appointment_Date varchar(10) not null,
+Appointment_Time varchar(10) not null,
+Patient_Name varchar(30) not null,
+Doctor_Name varchar(30) not null,
+Completed varchar(20) not null);"""
 
 
 
@@ -34,4 +83,5 @@ def create_database(connection, query):
 
 
 connection = create_server_connection("localhost", "root", "student", "Central_Clinic")
+execute_query(connection, create_table4)
 
