b979e33e23612b03c5a4c68c088a019de02ab95b
Your Name <you@example.com>
2022-09-28 09:16:01 -0500

    Authenticated DB within PyCharm.

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import mysql.connector
 from mysql.connector import Error
 
 
-def create_server_connection(host_name, user_name, user_password):
+def create_server_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
             password=user_password,
+            database = db_name
         )
         print("MySQL Database connection successful")
     except Error as err:
@@ -32,6 +33,5 @@ def create_database(connection, query):
 
 
 
-connection = create_server_connection("localhost", "root", "student")
-database_query = "CREATE DATABASE Central_Clinic"
-create_database(connection, database_query)
+connection = create_server_connection("localhost", "root", "student", "Central_Clinic")
+
